7ddb2c5cc7f455a83cccab0bf401b0c07ab92dcc
Tudor Hutu <hutu.tudor@yahoo.com>
2023-11-20 15:19:16 +0200

    chore: update storybook and react to v18

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,20 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(js|jsx|mdx)'],
+
   addons: [
     '@storybook/addon-docs',
     '@storybook/addon-actions',
     '@storybook/addon-links',
-    '@storybook/addon-viewport/register'
-  ]
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-mdx-gfm'
+  ],
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,29 @@
 import React from 'react'
 
-import { addDecorator } from '@storybook/react'
+import { StyleSheetManager } from 'styled-components'
 
 import { BrandingProvider, GlobalStyle, ThemeProvider } from '@etvas/etvaskit'
 import { buildTheme } from '@etvas/etvaskit'
 
 import { theme } from '../stories/theme'
 
-addDecorator(storyFn => (
-  <BrandingProvider>
-    <ThemeProvider theme={buildTheme(theme)}>
-      <GlobalStyle />
-      {storyFn()}
-    </ThemeProvider>
-  </BrandingProvider>
-))
+export const decorators = [
+  storyFn => (
+    <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+      <BrandingProvider>
+        <ThemeProvider theme={buildTheme(theme)}>
+          <GlobalStyle />
+          {storyFn()}
+        </ThemeProvider>
+      </BrandingProvider>
+    </StyleSheetManager>
+  )
+]
+
+function shouldForwardProp(propName, target) {
+  if (typeof target === 'string') {
+    return isPropValid(propName)
+  }
+
+  return true
+}
