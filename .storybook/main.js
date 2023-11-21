766c647a1705276981a1d63d740741df6d4c8de4
Radu-Mihai Castravet <devicedev@protonmail.com>
2023-11-21 12:25:22 +0100

    feat: update storybook to version 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,14 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(js|jsx|mdx)'],
-  addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-viewport/register'
-  ]
-}
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
+  features: {
+    postcss: false
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-import { addDecorator } from '@storybook/react'
-
 import { BrandingProvider, GlobalStyle, ThemeProvider } from '@etvas/etvaskit'
 import { buildTheme } from '@etvas/etvaskit'
 
 import { theme } from '../stories/theme'
 
-addDecorator(storyFn => (
-  <BrandingProvider>
-    <ThemeProvider theme={buildTheme(theme)}>
-      <GlobalStyle />
-      {storyFn()}
-    </ThemeProvider>
-  </BrandingProvider>
-))
+export const decorators = [
+  storyFn => (
+    <BrandingProvider>
+      <ThemeProvider theme={buildTheme(theme)}>
+        <GlobalStyle />
+        {storyFn()}
+      </ThemeProvider>
+    </BrandingProvider>
+  )
+]
