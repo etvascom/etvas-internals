5ea034e0290f55dfa78b1cfd87ec94f727f28cf9
Radu-Mihai Castravet <devicedev@protonmail.com>
2023-12-13 11:35:24 +0100

    refactor(RangePicker): Extract nested ternary

--- src/components/DatePicker/DateRangePicker.jsx
+++ src/components/DatePicker/DateRangePicker.jsx
@@ -194,13 +194,7 @@ const FakeInput = styled(Flex)(({ expanded, disabled, theme }) =>
     justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: disabled ? theme.colors.backgroundInputGray : 'white',
-    border: `1px solid ${
-      disabled
-        ? theme.colors.inputBorderGray
-        : expanded
-          ? theme.colors.accent
-          : theme.colors.inputBorderGray
-    }`,
+    border: `1px solid ${getDefaultBorderColor({ expanded, disabled, theme })}`,
     borderRadius: 2,
     cursor: disabled ? 'not-allowed' : 'pointer',
     pointerEvents: disabled ? 'none' : 'all',
@@ -209,6 +203,16 @@ const FakeInput = styled(Flex)(({ expanded, disabled, theme }) =>
     }
   })
 )
+
+const getDefaultBorderColor = ({ expanded, disabled, theme }) => {
+  if (disabled) {
+    return theme.colors.inputBorderGray
+  }
+  if (expanded) {
+    return theme.colors.accent
+  }
+  return theme.colors.inputBorderGray
+}
 
 const Wrapper = styled(Flex)`
   width: 100%;
